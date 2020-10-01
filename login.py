06d808cb95e43e1ffeea8a002369397c9972ed15
afrizaloky <afrizaloky@outlook.com>
2020-10-01 19:07:42 +0700

    refactor

--- login.py
+++ login.py
@@ -7,30 +7,38 @@ try:
         json_data = json.load(json_data_files)
         print("open file success")
     data = json_data["credentials"]
-    headers_2 = json_data["headers_2"]
-    headers_1 = json_data["headers_1"]
 except:
     print("open file error")
     pass
-username = name = input("username : ")
-password = getpass.getpass()
-data["username"] = username
-data["password"] = password
 
 
-session = requests.Session()
-response = session.get(
-    'https://sso.ugm.ac.id/cas/login', headers=headers_1)
-cookies = session.cookies.get_dict()
-contents = (response.content)
+def login():
+    username = input("username : ")
+    password = getpass.getpass()
+    data["username"] = username
+    data["password"] = password
 
-soup = BeautifulSoup(contents, "lxml")
-lt = soup.find("input", {'name': "lt"}).attrs['value']
+    session = requests.Session()
+    response = session.get(
+        'https://sso.ugm.ac.id/cas/login')
+    contents = (response.content)
 
-data["lt"] = lt
+    soup = BeautifulSoup(contents, "lxml")
+    lt = soup.find("input", {'name': "lt"}).attrs['value']
+
+    data["lt"] = lt
+
+    response = session.post('https://sso.ugm.ac.id/cas/login',
+                            data=data)
+    success = BeautifulSoup(response.text, 'html.parser')
+    div_success = success.find("div", attrs={"id": 'msg'})
+    # print(div_success)
+    if('alert alert-success' in str(div_success)):
+        return True
+    else:
+        return False
 
 
-response = session.post('https://sso.ugm.ac.id/cas/login',
-                        headers=headers_2, cookies=cookies, data=data)
-print(response.content)
-soup = BeautifulSoup(response.content, "lxml")
+if __name__ == "__main__":
+    if(login()):
+        print("You are logged in")
